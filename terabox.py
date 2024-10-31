d3e154fb701e14b327329d8f6b47e5b806333cb4
modapksfree <modapksfreeio@gmail.com>
2024-10-31 15:13:20 +0530

    Add files via upload

---
+++ terabox.py
@@ -0,0 +1,180 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+import logging
+import asyncio
+from datetime import datetime
+from pyrogram.enums import ChatMemberStatus
+from dotenv import load_dotenv
+from os import environ
+import os
+import time
+from status import format_progress_bar
+from video import download_video, upload_video
+from web import keep_alive
+
+#for token verification
+
+# terabox.py
+from shared import API, URL, check_token, get_token, verify_user, check_verification
+
+# constants.py
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/botsbyadmin/5") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "downloadteraboxvideobot") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
+
+
+load_dotenv('config.env', override=True)
+
+logging.basicConfig(level=logging.INFO)
+
+api_id = os.environ.get('TELEGRAM_API', '')
+if len(api_id) == 0:
+    logging.error("TELEGRAM_API variable is missing! Exiting now")
+    exit(1)
+
+api_hash = os.environ.get('TELEGRAM_HASH', '')
+if len(api_hash) == 0:
+    logging.error("TELEGRAM_HASH variable is missing! Exiting now")
+    exit(1)
+    
+bot_token = os.environ.get('BOT_TOKEN', '')
+if len(bot_token) == 0:
+    logging.error("BOT_TOKEN variable is missing! Exiting now")
+    exit(1)
+dump_id = os.environ.get('DUMP_CHAT_ID', '')
+if len(dump_id) == 0:
+    logging.error("DUMP_CHAT_ID variable is missing! Exiting now")
+    exit(1)
+else:
+    dump_id = int(dump_id)
+
+fsub_id = os.environ.get('FSUB_ID', '')
+if len(fsub_id) == 0:
+    logging.error("FSUB_ID variable is missing! Exiting now")
+    exit(1)
+else:
+    fsub_id = int(fsub_id)
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+@app.on_message(filters.command("start"))
+async def start_command(client, message):
+    # Check if the command has at least one argument
+    if len(message.command) > 1:
+        data = message.command[1]
+        
+        if data.split("-", 1)[0] == "verify":  # Assuming this is a valid case
+            userid = data.split("-", 2)[1]
+            token = data.split("-", 3)[2]
+            
+            if str(message.from_user.id) != str(userid):
+                return await message.reply_text(
+                    text="<b>Invalid link or Expired link!</b>",
+                    protect_content=True
+                )
+            
+            is_valid = await check_token(client, userid, token)
+            if is_valid:
+                await message.reply_text(
+                    text=f"<b>Hey {message.from_user.mention}, You are successfully verified!\nNow you have unlimited access for all files till today midnight.</b>",
+                    protect_content=True
+                )
+                await verify_user(client, userid, token)
+            else:
+                return await message.reply_text(
+                    text="<b>Invalid link or Expired link!</b>",
+                    protect_content=True
+                )
+   
+    sticker_message = await message.reply_sticker("CAACAgUAAxkBAAEuTL9nBqKO6TPOhJY2DPq3CxQSZ6a6JgACaxIAAu8VOVSTXlm2jC30LTYE")
+    await asyncio.sleep(2)
+    await sticker_message.delete()
+    
+    user_mention = message.from_user.mention
+    reply_message = f"ᴡᴇʟᴄᴏᴍᴇ, {user_mention}.\n\n🌟 ɪ ᴀᴍ ᴀ ᴛᴇʀᴀʙᴏx ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ʙᴏᴛ. sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴛᴇʀᴀʙᴏx ʟɪɴᴋ ɪ ᴡɪʟʟ ᴅᴏᴡɴʟᴏᴀᴅ ᴡɪᴛʜɪɴ ғᴇᴡ sᴇᴄᴏɴᴅs ᴀɴᴅ sᴇɴᴅ ɪᴛ ᴛᴏ ʏᴏᴜ ✨."
+    
+    join_button = InlineKeyboardButton("ᴊᴏɪɴ ❤️🚀", url="https://t.me/freeteradownloader")
+    developer_button = InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ⚡️", url="https://t.me/botsbyadmin")
+    reply_markup = InlineKeyboardMarkup([[join_button, developer_button]])
+    
+    video_file_id = "/app/freeteradownloader.jpg"
+    if os.path.exists(video_file_id):
+        await client.send_video(
+            chat_id=message.chat.id,
+            video=video_file_id,
+            caption=reply_message,
+            reply_markup=reply_markup
+        )
+    else:
+        await message.reply_text(reply_message, reply_markup=reply_markup)
+
+async def is_user_member(client, user_id):
+    try:
+        member = await client.get_chat_member(fsub_id, user_id)
+        logging.info(f"User {user_id} membership status: {member.status}")
+        if member.status in [ChatMemberStatus.MEMBER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.error(f"Error checking membership status for user {user_id}: {e}")
+        return False
+
+@app.on_message(filters.text)
+async def handle_message(client, message: Message):
+
+    user_id = message.from_user.id
+    user_mention = message.from_user.mention
+
+    # Check if the user is a member of the channel
+    is_member = await is_user_member(client, user_id)
+# Check if the user is verified
+    if not await check_verification(client, message.from_user.id) or not VERIFY:
+        btn = [
+            [
+                InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{BOT_USERNAME}?start="))
+            ],
+            [
+                InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
+            ]
+        ]
+        await message.reply_text(
+            text="<b>You are not verified!\nKindly verify to continue!</b>",
+            protect_content=True,
+            reply_markup=InlineKeyboardMarkup(btn)
+        )
+        return
+    if not is_member:
+        join_button = InlineKeyboardButton("ᴊᴏɪɴ ❤️🚀", url="https://t.me/freeteradownloader")
+        reply_markup = InlineKeyboardMarkup([[join_button]])
+        await message.reply_text("ʏᴏᴜ ᴍᴜsᴛ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ.", reply_markup=reply_markup)
+        return
+
+    
+
+    # Additional code to handle verified and member users...
+
+    valid_domains = [
+    'terabox.com', 'nephobox.com', '4funbox.com', 'mirrobox.com', 
+    'momerybox.com', 'teraboxapp.com', '1024tera.com', 
+    'terabox.app', 'gibibox.com', 'goaibox.com', 'terasharelink.com', 'teraboxlink.com'
+    ]
+
+    terabox_link = message.text.strip()
+
+    if not any(domain in terabox_link for domain in valid_domains):
+        await message.reply_text("ᴘʟᴇᴀsᴇ sᴇɴᴅ ᴀ ᴠᴀʟɪᴅ ᴛᴇʀᴀʙᴏx ʟɪɴᴋ.")
+        return
+
+    reply_msg = await message.reply_text("sᴇɴᴅɪɴɢ ʏᴏᴜ ᴛʜᴇ ᴍᴇᴅɪᴀ...🤤")
+
+    try:
+        file_path, thumbnail_path, video_title = await download_video(terabox_link, reply_msg, user_mention, user_id)
+        await upload_video(client, file_path, thumbnail_path, video_title, reply_msg, dump_id, user_mention, user_id, message)
+    except Exception as e:
+        logging.error(f"Error handling message: {e}")
+        await reply_msg.edit_text("Api has given a Broken Download Link. Dont Contact the Owner for this Issue.")
+
+if __name__ == "__main__":
+    keep_alive()
+    app.run()
---
+++ utils.py
@@ -0,0 +1,9 @@
+import pytz, random, string  
+from datetime import date 
+from terabox import API, URL
+from shortzy import Shortzy
+
+# utils.py
+from shared import check_verification, check_token, get_token
+
+
